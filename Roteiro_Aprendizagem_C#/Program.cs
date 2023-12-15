e816416b1572f4ef2eae331ed56593d24eadd1ea
Felipe Santos <felipesantossf@outlook.com>
2023-12-15 16:01:54 -0300

    Adicionar arquivos de projeto.

---
+++ Roteiro_Aprendizagem_C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roteiro_Aprendizagem_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
