a77d6beaaf43f43c441ebf014456016054985d1c
Felipe Santos <felipesantossf@outlook.com>
2023-12-15 17:22:34 -0300

    Adicionando Sequências de escape de caractere \n e \t

--- Roteiro_Aprendizagem_C#/Program.cs
+++ Roteiro_Aprendizagem_C#/Program.cs
@@ -10,7 +10,13 @@ namespace Roteiro_Aprendizagem_C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testando");
+            Console.WriteLine("Hello World!");
+
+            // Adicionando Sequências de escape de caractere 
+            // Uma sequência de caracteres de escape é uma instrução especial para que o runtime insira um caractere especial que afetará a saída da cadeia de caracteres.
+
+            Console.WriteLine("escape\nespecial"); // \n adicionára uma nova linha 
+            Console.WriteLine("escape\tespecial");
 
             Console.ReadKey();
         }
