5190e579db752d38a1a95c1bbd77d65e3ba65842
Felipe Santos <felipesantossf@outlook.com>
2023-12-16 08:46:06 -0300

    Exercicio - invertendo uma string e contando o número de vezes que um caractere em especifico aparece

--- Roteiro_Aprendizagem_C#/Program.cs
+++ Roteiro_Aprendizagem_C#/Program.cs
@@ -104,6 +104,32 @@ namespace Roteiro_Aprendizagem_C_
                 Console.WriteLine(orderID);
             }
 
+            /*
+                This code reverses a message, count the number of times
+                a particular character appears, then print the results
+                to the console window.
+             */
+
+            string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+            char[] message = originalMessage.ToCharArray();
+            Array.Reverse(message);
+
+            int letterCount = 0;
+
+            foreach (char i in message)
+            {
+                if (i == 'o')
+                {
+                    letterCount++;
+                }
+            }
+
+            string newMessage = new string(message);
+
+            Console.WriteLine(newMessage);
+            Console.WriteLine($"The letter 'o' appears {letterCount} times.");
+
             Console.ReadKey();
         }
 
