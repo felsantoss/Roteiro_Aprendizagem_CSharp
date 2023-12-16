e45bd5a0eb9f7945c4c3d33af3daa3d1c442e4a8
Felipe Santos <felipesantossf@outlook.com>
2023-12-16 11:13:01 -0300

    Projeto finalizado

--- ProjetosGuiado/Exercicios.cs
+++ ProjetosGuiado/Exercicios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,29 +12,132 @@ namespace ProjetosGuiado
         static void Main(string[] args)
         {
             // initialize variables - graded assignments 
-            int currentAssignments = 5;
+            int examAssignments = 5;
 
-            int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-            int[] andrewScore = new int[] { 92, 89, 81, 96, 90 };            
-            int[] emmaScore = new int[] { 90, 85, 87, 98, 68 };
-            int[] loganScore = new int[] { 90, 95, 87, 88, 96 };
+            int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+            int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+            int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+            int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+            int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+            int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+            int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+            int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
-            int sophiaSum = 0;
-            
-            decimal sophiaScore;
+            // Student names
+            string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
-            foreach (int score in sophiaScores)
-            {
-                // add the exam score to the sum
-                sophiaSum += score;
-            }
-                        
-            sophiaScore = (decimal)sophiaSum / currentAssignments;
-            
+            int[] studentScores = new int[10];
+
+            string currentStudentLetterGrade = "";
+
+            // Write the Report Header to the console
             Console.WriteLine("Student\t\tGrade\n");
-            Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-            
-            Console.WriteLine("Press the any key to continue");
+
+            foreach (string name in studentNames)
+            {
+                string currentStudent = name;
+
+                if (currentStudent == "Sophia")
+                    // assign Sophia's scores to the studentScores array
+                    studentScores = sophiaScores;
+
+                else if (currentStudent == "Andrew")
+                    // assign Andrew's scores to the studentScores array 
+                    studentScores = andrewScores;
+
+                else if (currentStudent == "Emma")
+                    // assign Emma's scores to the studentScores array 
+                    studentScores = emmaScores;
+
+                else if (currentStudent == "Logan")
+                    // assign Logan's scores to the studentScores array 
+                    studentScores = loganScores;
+
+                else if (currentStudent == "Becky")
+                    studentScores = beckyScores;
+
+                else if (currentStudent == "Chris")
+                    studentScores = chrisScores;
+
+                else if (currentStudent == "Eric")
+                    studentScores = ericScores;
+
+                else if (currentStudent == "Gregor")
+                    studentScores = gregorScores;
+
+                else
+                    continue;
+
+                // Initialize/reset the sum of scored assignments
+                int sumAssignmentScores = 0;
+
+                // Initialize/reset the calculated average of exam + extra credit scores
+                decimal currentStudentGrade = 0;
+
+                // initialize/reset a counter for the number of assignments
+                int gradedAssignments = 0;
+
+                foreach (int score in studentScores)
+                {
+                    // increment the assignment counter
+                    gradedAssignments += 1;
+
+                    if (gradedAssignments <= examAssignments)
+                        // add the exam score to the sum
+                        sumAssignmentScores += score;
+
+                    else
+                        // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+                        sumAssignmentScores += score / 10;
+                }
+
+                currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+
+                if (currentStudentGrade >= 97)
+                    currentStudentLetterGrade = "A+";
+
+                else if (currentStudentGrade >= 93)
+                    currentStudentLetterGrade = "A";
+
+                else if (currentStudentGrade >= 90)
+                    currentStudentLetterGrade = "A-";
+
+                else if (currentStudentGrade >= 87)
+                    currentStudentLetterGrade = "B+";
+
+                else if (currentStudentGrade >= 83)
+                    currentStudentLetterGrade = "B";
+
+                else if (currentStudentGrade >= 80)
+                    currentStudentLetterGrade = "B-";
+
+                else if (currentStudentGrade >= 77)
+                    currentStudentLetterGrade = "C+";
+
+                else if (currentStudentGrade >= 73)
+                    currentStudentLetterGrade = "C";
+
+                else if (currentStudentGrade >= 70)
+                    currentStudentLetterGrade = "C-";
+
+                else if (currentStudentGrade >= 67)
+                    currentStudentLetterGrade = "D+";
+
+                else if (currentStudentGrade >= 63)
+                    currentStudentLetterGrade = "D";
+
+                else if (currentStudentGrade >= 60)
+                    currentStudentLetterGrade = "D-";
+
+                else 
+                    currentStudentLetterGrade = "F";
+
+
+                Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+
+            }
+
+            Console.WriteLine("\nPress the any key to continue");
             Console.ReadLine();
 
             
