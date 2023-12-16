f89e98892981f0d9f5a196f3e7690a9e2374d0f8
Felipe Santos <felipesantossf@outlook.com>
2023-12-16 09:09:11 -0300

    Criação de uma nova pasta para realizar os projetos guiados da plataforma - https://learn.microsoft.com/pt-br/training/browse/?products=dotnet&resource_type=learning%20path&roles=developer

---
+++ ProjetosGuiado/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosGuiado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNormal = {3, 2, 1};
+
+            Array.Sort(arrayNormal);
+
+            Console.WriteLine(arrayNormal); 
+
+            Console.ReadKey();
+        }
+    }
+}
