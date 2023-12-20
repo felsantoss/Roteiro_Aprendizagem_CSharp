195b471c0ab46d048d979c54f0ccc2cbdf6aa30e
Felipe Santos <felipesantossf@outlook.com>
2023-12-20 11:35:12 -0300

    Operador condicional ?: <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>

--- Roteiro Parte 2/Program.cs
+++ Roteiro Parte 2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.Authentication;
 using System.Text;
@@ -11,28 +12,36 @@ namespace Roteiro_Parte_2
     {
         static void Main(string[] args)
         {
-            // string value1 = " a";
-            // string value2 = "A ";
+            string value1 = " a";
+            string value2 = "A ";
 
-            // Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+            Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-            // Console.WriteLine("a" != "a");
-            // Console.WriteLine("a" != "A");
-            // Console.WriteLine(1 != 2);
+            Console.WriteLine("a" != "a");
+            Console.WriteLine("a" != "A");
+            Console.WriteLine(1 != 2);
 
-            // string myValue = "a";
+            string myValue = "a";
 
-            // Console.WriteLine(myValue != "a");
+            Console.WriteLine(myValue != "a");
 
-            //Console.WriteLine(1 > 2);
-            //Console.WriteLine(1 < 2);
-            //Console.WriteLine(1 >= 1);
-            //Console.WriteLine(1 <= 1);  
+            Console.WriteLine(1 > 2);
+            Console.WriteLine(1 < 2);
+            Console.WriteLine(1 >= 1);
+            Console.WriteLine(1 <= 1);  
 
             string pangram = "The quick brown fox jumps over the lazy dog.";
 
-            Console.WriteLine(!pangram.Contains("fox"));
+            Console.WriteLine(pangram.Contains("fox"));
             Console.WriteLine(!pangram.Contains("cow"));
+
+            // operador condicional ?: 
+            // <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+            int saleAmount = 1001;
+            // int discount = saleAmount > 1000 ? 100 : 50;
+            
+            Console.WriteLine($"\nDiscount: {(saleAmount > 1000 ? 100 : 50)}");
 
             Console.WriteLine("\nPress any key to close");
             Console.ReadKey();
