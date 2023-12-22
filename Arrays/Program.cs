919e992b17be27a1d22650fb353bc7ff06b67332
Felipe Santos <felipesantossf@outlook.com>
2023-12-22 14:44:50 -0300

    Criação do array dvdCollection com capacidade para 15 unidades

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,8 +8,30 @@ namespace Arrays
 {
     internal class Program
     {
+        DVD[] dvdCollection = new DVD[15];
+
+        public class DVD
+        {
+            public string name;
+            public int releaseYear;
+            public string director;
+
+            public DVD(String name, int releaseYear, string director)
+            {
+                this.name = name;
+                this.releaseYear = releaseYear;
+                this.director = director;
+            }
+
+            public string toString()
+            {
+                return this.name + ", directed by " + this.director + ", release in " + this.releaseYear;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.ReadKey();
         }
     }
 }
