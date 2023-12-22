9fc25f0e36c1fb0fa4a4a2509b2e72462edaab16
Felipe Santos <felipesantossf@outlook.com>
2023-12-22 13:55:57 -0300

    atividade switch case

--- Roteiro Parte 2/Program.cs
+++ Roteiro Parte 2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Security.Authentication;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -143,6 +145,73 @@ namespace Roteiro_Parte_2
             }
 
             Console.WriteLine($"{employeeName}, {title}");
+
+            // SKU = Stock Keeping Unit. 
+            // SKU value format: <product #>-<2-letter color code>-<size code>
+
+            string sku = "01-MN-L";
+
+            string[] product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+
+            switch (product[0])
+            {
+                case "01":
+                    type = "Sweat shirt";
+                    break;
+
+                case "02":
+                    type = "T-Shirt";
+                    break;
+
+                case "03":
+                    type = "Sweat pants";
+                    break;
+
+                default:
+                    type = "Other";
+                    break;
+            }
+
+            switch (product[1])
+            {
+                case "BL":
+                    color = "Black";
+                    break;
+
+                case "MN":
+                    color = "Maroon";
+                    break;
+
+                default:
+                    color = "White";
+                    break;
+            }
+            
+            switch (product[2])
+            {
+                case "S":
+                    size = "Small";
+                    break;
+
+                case "M":
+                    size = "Medium";
+                    break;
+
+                case "L":
+                    size = "Large";
+                    break;
+
+                default:
+                    size = "One Size Fits All";
+                    break;
+            }
+            
+          
+            Console.WriteLine($"Product: {size} {color} {type}");
 
             Console.WriteLine("\nPress any key to close");
             Console.ReadKey();
