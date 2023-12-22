5661a44985aed73aa23c9e780452407b82bbc0de
Felipe Santos <felipesantossf@outlook.com>
2023-12-22 17:14:55 -0300

    Inserindo e imprimendo informações do array no Console

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,6 @@ namespace Arrays
 {
     internal class Program
     {
-        DVD[] dvdCollection = new DVD[15];
-
         public class DVD
         {
             public string name;
@@ -23,15 +22,44 @@ namespace Arrays
                 this.director = director;
             }
 
-            public string toString()
+            public string ToString()
             {
                 return this.name + ", directed by " + this.director + ", release in " + this.releaseYear;
             }
+
         }
+
 
         static void Main(string[] args)
         {
+            DVD[] dvdCollection = new DVD[15];
+            
+            DVD avengersDVD = new DVD("The Avengers", 2012, "Joss Whedon");
+
+            dvdCollection[7] = avengersDVD;
+
+            DVD incrediblesDVD = new DVD("The Incredibles", 2004, "Brad Bird");
+            DVD findingDoryDVD = new DVD("Finding Dory", 2016, "Andrew Stanton");
+            DVD lionKingDVD = new DVD("The Lion King", 2019, "Jon Favreau");
+
+            // Put "The Incredibles" into the 4th place: index 3.
+            dvdCollection[3] = incrediblesDVD;
+
+            // Put "Finding Dory" into the 10th place: index 9.
+            dvdCollection[9] = findingDoryDVD;
+
+            // Put "The Lion King" into the 3rd place: index 2.
+            dvdCollection[2] = lionKingDVD;
+
+            DVD starWarsDVD = new DVD("Star Wars", 1977, "George Lucas");
+            dvdCollection[3] = starWarsDVD;
+
+            Console.WriteLine(dvdCollection[7].ToString());
+            Console.WriteLine(dvdCollection[2].ToString());
+            Console.WriteLine(dvdCollection[3].ToString());
+
             Console.ReadKey();
+
         }
     }
 }
