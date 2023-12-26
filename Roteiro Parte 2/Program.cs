95780491022c65632042b558c8c3c9a61ac6cf6c
Felipe Santos <felipesantossf@outlook.com>
2023-12-26 19:00:11 -0300

    utilizando a palavra-chave continue. A primeira coisa que fazemos dentro do bloco de código é atribuir um novo valor aleatório a current. Em seguida, verificamos se current é maior ou igual a 8. Se essa expressão retornar true, a palavra-chave continue transferirá o controle para o final do bloco de código e while será avaliada como (current != 7). Assim, o loop continuará iterando enquanto o valor de current não for igual a 7

--- Roteiro Parte 2/Program.cs
+++ Roteiro Parte 2/Program.cs
@@ -273,13 +273,28 @@ namespace Roteiro_Parte_2
             Console.WriteLine("\nExample of do-while");
 
             Random random = new Random();
-            int current = 0;
+            int current = random.Next(1, 11);
 
             do
             {
                 current = random.Next(1, 11);
+
+                if (current >= 8)
+                    continue;
+
                 Console.WriteLine(current);
-            } while (current != 7);
+            } 
+            while (current != 7);
+
+
+
+            //while (current >= 3)
+            //{
+            //    Console.WriteLine(current);
+            //    current = random.Next(1, 11);
+            //}
+
+            //Console.WriteLine($"Last number: {current}");
 
             Console.WriteLine("\nPress any key to close");
             Console.ReadKey();
