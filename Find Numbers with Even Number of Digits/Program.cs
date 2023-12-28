fb6c831fe3463c9a949491d896d0e1660413dd0a
Felipe Santos <felipesantossf@outlook.com>
2023-12-28 14:19:10 -0300

    Resolução do problema Squares of a Sorted Array. Onde precisava elevar os numeros de um array ao quadrado e ordenar por ordem crescente

---
+++ Find Numbers with Even Number of Digits/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Numbers_with_Even_Number_of_Digits
+{
+    internal class Program
+    {
+        // Given an array nums of integers, return how many of them contain an even number of digits.
+        static void Main(string[] args)
+        {
+            // Example of how to find numbers of digits 
+
+            int number = 2;
+
+            int numberOfDigits = 0;
+
+            while (number != 0)
+            {
+                number /= 10;
+                numberOfDigits++;
+            }
+
+            if (numberOfDigits % 2 == 0)
+                Console.WriteLine($"Even number, total of digits: {numberOfDigits}");
+            else
+                Console.WriteLine($"Odd number, total of digits: {numberOfDigits}");
+
+            int findNumbers(int[] nums)
+            {
+                int count = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int arrayNumber = nums[i];
+
+                    string numString = arrayNumber.ToString();
+
+                    if (numString.Length % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            int[] arr = { 12, 345, 2, 6, 7896 };
+
+            Console.WriteLine(findNumbers(arr));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squares_of_a_Sorted_Array
+{
+    internal class Program
+    {
+        // Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] SortedSquares(int[] nums)
+            {
+                int[] newArray = new int[nums.Length];
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int newNumber = nums[i] * nums[i];
+
+                    newArray[i] = newNumber;
+                }
+
+                int[] arraySorted = newArray.OrderBy(i => i).ToArray();
+
+                return arraySorted;
+            }
+
+            int[] arr = { -7, -3, 2, 3, 11 };
+            int[] squareArray = SortedSquares(arr);
+
+            Console.WriteLine(string.Join(", ", squareArray));
+
+            Console.ReadKey();
+        }
+    }
+}
