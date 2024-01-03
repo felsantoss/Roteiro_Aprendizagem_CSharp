53d01b64dccf74788ff8e986eb0717ca4c39dab1
Felipe Santos <felipesantossf@outlook.com>
2024-01-03 10:45:01 -0300

    Tipo integram sem sinal: Um tipo sem sinal usa seus bytes para representar apenas números positivos.

--- TipoDeDados/Program.cs
+++ TipoDeDados/Program.cs
@@ -17,6 +17,14 @@ namespace TipoDeDados
             Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
             Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
 
+            Console.WriteLine("");
+            Console.WriteLine("Tipo integral sem sinal");
+
+            Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+            Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+            Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+            Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
             Console.ReadKey();
         }
     }
