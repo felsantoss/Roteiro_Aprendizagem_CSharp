68b544ab98116149abf0c71baa0289be568d4be3
Felipe Santos <felipesantossf@outlook.com>
2024-01-03 10:17:17 -0300

    Tipo integral com sinal :  Um tipo com sinal usa seus bytes para representar uma quantidade igual de números positivos e negativos. Quanto cada tipo de dado consegue armazenar

---
+++ TipoDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Signed integral types:");
+
+            Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+            Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+            Console.ReadKey();
+        }
+    }
+}
