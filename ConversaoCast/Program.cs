402dc43fe8e9c54b7771e14b35ec9dd18d5c9f02
Felipe Santos <felipesantossf@outlook.com>
2024-01-04 06:02:29 -0300

    Para executar uma coerção, use o operador de coerção () para cercar um tipo de dados e coloque-o ao lado da variável que deseja converter (por exemplo: (int)myDecimal). Você executará uma conversão explícita para o tipo de dados de coerção definido (int).

--- ConversaoCast/Program.cs
+++ ConversaoCast/Program.cs
@@ -14,13 +14,21 @@ namespace ConversaoCast
             /* int first = 2;
             string second = "4";
             string result = first + second;
-            Console.WriteLine(result); */
+            Console.WriteLine(result); 
 
             int myInt = 3;
             Console.WriteLine($"int: {myInt}");
 
             decimal myDecimal = myInt;
+            Console.WriteLine($"decimal: {myDecimal}"); */
+
+
+            // Coerção
+            decimal myDecimal = 3.14m;
             Console.WriteLine($"decimal: {myDecimal}");
+
+            int myInt = (int)myDecimal;
+            Console.WriteLine($"int: {myInt}");
 
             Console.ReadKey();
         }
