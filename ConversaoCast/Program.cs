30d08fc587628c3e50af6a8e38c870f327b9c709
Felipe Santos <felipesantossf@outlook.com>
2024-01-04 06:19:24 -0300

    Convertendo numero para String com ToString() Convertendo String para numero usando Parse()

--- ConversaoCast/Program.cs
+++ ConversaoCast/Program.cs
@@ -20,15 +20,38 @@ namespace ConversaoCast
             Console.WriteLine($"int: {myInt}");
 
             decimal myDecimal = myInt;
-            Console.WriteLine($"decimal: {myDecimal}"); */
+            Console.WriteLine($"decimal: {myDecimal}"); 
 
 
-            // Coerção
+            Console.WriteLine("\nConversão por Coerção");
             decimal myDecimal = 3.14m;
             Console.WriteLine($"decimal: {myDecimal}");
 
-            int myInt = (int)myDecimal;
-            Console.WriteLine($"int: {myInt}");
+            int myInt = (int)myDecimal; // conversão feita por Coerção, colocando () ao lado da variavel
+            Console.WriteLine($"int: {myInt}"); 
+
+
+            Console.WriteLine("\nconversão de Restrição"); // você pode perder informações como precisão (ou seja, o número de valores após o ponto decimal).
+            decimal myDecimal = 1.23456789m;
+            float myFloat = (float)myDecimal;
+
+            Console.WriteLine($"Decimal: {myDecimal}");
+            Console.WriteLine($"Float  : {myFloat}"); 
+
+            Console.WriteLine("\nConvertendo número para String usando ToString()");
+            int first = 5;
+            int second = 7;
+            string message = first.ToString() + second.ToString();
+
+            Console.WriteLine(message); */
+
+            Console.WriteLine("\nConvertendo String em Int usando Parse()");
+            string first = "5";
+            string second = "7";
+            int sum = int.Parse(first) + int.Parse(second);
+
+            Console.WriteLine(sum);
+
 
             Console.ReadKey();
         }
