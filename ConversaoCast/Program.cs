57cde9a75833721fa83435d3ae5e54aed4e2691e
Felipe Santos <felipesantossf@outlook.com>
2024-01-04 14:51:10 -0300

    Conversão Cast

--- ConversaoCast/Program.cs
+++ ConversaoCast/Program.cs
@@ -85,7 +85,7 @@ namespace ConversaoCast
             if (result > 0)
                 Console.WriteLine($"Resultado (c/ desvio): {50 + result}"); */
 
-            string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+            /* string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
             decimal total = 0m;
             string message = "";
@@ -104,9 +104,24 @@ namespace ConversaoCast
             }
 
             Console.WriteLine($"Message: {message}");
-            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Total: {total}"); */
 
-            Console.ReadKey();
+            int value1 = 12;
+            decimal value2 = 6.2m;
+            float value3 = 4.3f;
+
+
+            int result1 = Convert.ToInt32(value1 / value2);
+            // Hint: You need to round the result to nearest integer (don't just truncate)
+            Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+            decimal result2 = value2 / (decimal)value3;
+            Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+            float result3 = value3 / (float)value1;
+            Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+            Console.ReadKey();  
         }
     }
 }
