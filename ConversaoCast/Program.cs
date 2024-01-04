6ba3743c015b5aa084d9c94a6933345d06c5dc2d
Felipe Santos <felipesantossf@outlook.com>
2024-01-04 05:57:09 -0300

    O compilador não consegue converter uma string para uma int, mas consegue converter uma int para uma string e concatenar o resultado

---
+++ ConversaoCast/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversaoCast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int first = 2;
+            string second = "4";
+            int result = first + second;
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
