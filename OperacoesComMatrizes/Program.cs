d5db240aafdce6217fb037dbbce066f0b750cdc7
Felipe Santos <felipesantossf@outlook.com>
2024-01-05 14:09:02 -0300

    Método Clear() limpar valores do array Método Resize() alterar o tamanho do array

--- OperacoesComMatrizes/Program.cs
+++ OperacoesComMatrizes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace OperacoesComMatrizes
             string[] pallets = { "B14", "A11", "B12", "A13" };
 
             Console.WriteLine("Sorted...");
-            Array.Sort(pallets);
+            Array.Sort(pallets); // Classifica os itens em ordem crescente 
 
             foreach (var pallet in pallets)
             {
@@ -21,7 +22,29 @@ namespace OperacoesComMatrizes
             }
 
             Console.WriteLine("\nReversed...");
-            Array.Reverse(pallets);
+            Array.Reverse(pallets); // Interve a ordem dos itens 
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine($"Before: {pallets[0]}");
+            Array.Clear(pallets, 0, 2); // Método para limpar valores (array, i inicial, n i para limpar)
+            Console.WriteLine($"After: {pallets[0]}");
+
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Array.Resize(ref pallets, 6); // altera o tamanho de uma matriz para o tamanho especificado
+            Console.WriteLine($"\nResizing 6 ... count: {pallets.Length}");
+
+            pallets[4] = "C01";
+            pallets[5] = "C02";
 
             foreach (var pallet in pallets)
             {
