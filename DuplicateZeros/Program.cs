f36f57930b073c3a9408b42cba9300766928b86a
Felipe Santos <felipesantossf@outlook.com>
2024-01-05 10:03:59 -0300

    Resolvendo o problema: Duplicate Zero LeetCode

---
+++ DuplicateZeros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateZeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void DuplicateZeros(int[] arr)
+            {
+                for (int i = 0;  i < arr.Length - 1; i++)
+                {
+                    if (arr[i] == 0)
+                    {
+                        for (int j = arr.Length - 1; j > i; j--)
+                        {
+                            arr[j] = arr[j - 1];
+                        }
+                        i++;
+                    }
+                    
+                }
+            }
+
+            int[] array = { 1, 0, 2, 3, 0, 4, 5, 0 };
+
+            DuplicateZeros(array);
+
+            foreach (int var in array)
+            {
+                Console.WriteLine(var);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
