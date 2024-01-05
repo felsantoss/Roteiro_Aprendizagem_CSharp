852d3f0d2e09b1375a9c819d0e3872880ee5f489
Felipe Santos <felipesantossf@outlook.com>
2024-01-05 13:46:09 -0300

    Método Sort() da classe Array é usado para order os itens em ordem crescente

---
+++ OperacoesComMatrizes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacoesComMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pallets = { "B14", "A11", "B12", "A13" };
+
+            Console.WriteLine("Sorted...");
+            Array.Sort(pallets);
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
