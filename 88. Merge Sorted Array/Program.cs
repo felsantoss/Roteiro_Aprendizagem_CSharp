2b81585ed80f5b5cb97bfe9cfd40fbb5ae0b16c0
Felipe Santos <felipesantossf@outlook.com>
2024-01-08 19:28:35 -0300

    Criação do projeto para resolução do desafio 88. Merge Sorted Array. Link: https://leetcode.com/problems/merge-sorted-array/

---
+++ 88. Merge Sorted Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _88.Merge_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Merge(int[] nums1, int m, int[] nums2, int n)
+            {
+                int[] finalArray = new int[nums1.Length + m + n];
+            }
+
+            int[] firstArr = { 1, 2, 3, 0, 0, 0 };
+            int[] secondArr = { 2, 5, 6 };
+
+            Console.ReadKey();
+        }
+    }
+}
