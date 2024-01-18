82c2b59ee395aeee6f7a9d9bd49d8b4d78759e9c
felsantoss <felipesantossf@outlook.com>
2024-01-18 20:28:08 -0300

    Formatação de números usando :N

--- FormatarDadosAlfanumericos/Program.cs
+++ FormatarDadosAlfanumericos/Program.cs
@@ -1,4 +1,5 @@
-﻿string first = "Hello";
+﻿// interpolação de caracteres
+string first = "Hello";
 string second = "World";
 Console.WriteLine($"{first} {second}!");
 Console.WriteLine($"{second} {first}!");
@@ -7,5 +8,8 @@ Console.WriteLine($"{first} {first} {first}!");
 // Formatação de moeda
 decimal price = 123.45m;
 int discount = 50;
-
 Console.WriteLine($"\nPrice: {price:C} (Save: {discount:C})");
+
+// Formatação de números
+decimal measurement = 123456.78912m;
+Console.WriteLine($"\nMeasurement: {measurement:N} units");
