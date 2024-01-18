2a5c1b166862909bb75abed48b5862d49d0bc4be
felsantoss <felipesantossf@outlook.com>
2024-01-18 20:15:20 -0300

    interpolação de caracteres

--- FormatarDadosAlfanumericos/Program.cs
+++ FormatarDadosAlfanumericos/Program.cs
@@ -1,5 +1,5 @@
 ﻿string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-
-Console.WriteLine(result);
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
