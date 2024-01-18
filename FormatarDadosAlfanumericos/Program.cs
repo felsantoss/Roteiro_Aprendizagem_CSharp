5f438a21c4b9c76cf8f2cfdfc4ee30e06ce88088
felsantoss <felipesantossf@outlook.com>
2024-01-18 20:23:07 -0300

    Formatação de moeda usando :C É formatado para a linguagem que estiver sendo usado no windows, neste caso para R$

--- FormatarDadosAlfanumericos/Program.cs
+++ FormatarDadosAlfanumericos/Program.cs
@@ -3,3 +3,9 @@ string second = "World";
 Console.WriteLine($"{first} {second}!");
 Console.WriteLine($"{second} {first}!");
 Console.WriteLine($"{first} {first} {first}!");
+
+// Formatação de moeda
+decimal price = 123.45m;
+int discount = 50;
+
+Console.WriteLine($"\nPrice: {price:C} (Save: {discount:C})");
