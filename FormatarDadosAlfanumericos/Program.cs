0bbbaa6e28e36ae90c4fb61c49eb3d00e14acfbc
felsantoss <felipesantossf@outlook.com>
2024-01-19 14:37:16 -0300

    desafio da formatação

--- FormatarDadosAlfanumericos/Program.cs
+++ FormatarDadosAlfanumericos/Program.cs
@@ -41,7 +41,7 @@ Console.WriteLine($"Total billed: {total:C}");
 
 string input = "Pad this";
 Console.WriteLine(input.PadLeft(12, '-'));
-Console.WriteLine(input.PadRight(12, '-')); */
+Console.WriteLine(input.PadRight(12, '-')); 
 
 string paymentId = "769C";
 string payeeName = "Mr. Felipe Santos";
@@ -52,6 +52,38 @@ formattedLine += payeeName.PadRight(24);
 formattedLine += paymentAmount.PadLeft(10);
 
 Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formattedLine);
+Console.WriteLine(formattedLine); */
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine($"\nCurrently, you own {currentShares:N} shares at a return of {currentReturn:P2}.");
+Console.WriteLine($"\nOur new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.");
+
+Console.WriteLine("\nHere's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+
+Console.WriteLine(comparisonMessage);
 
 Console.ReadKey();
