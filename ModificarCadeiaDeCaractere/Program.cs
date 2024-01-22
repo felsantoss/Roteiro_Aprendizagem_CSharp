01811633b5f3ff2f312c739d8694880aa1da67a2
felsantoss <felipesantossf@outlook.com>
2024-01-22 18:26:32 -0300

    Utilizando while para percorrer uma string para separar todas as frases em parenteses

--- ModificarCadeiaDeCaractere/Program.cs
+++ ModificarCadeiaDeCaractere/Program.cs
@@ -1,4 +1,4 @@
-﻿string message = "What is the value <span>between the tags</span>";
+﻿/* string message = "What is the value <span>between the tags</span>";
 
 const string openSpan = "<span>";
 const string closeSpan = "</span>";
@@ -9,6 +9,23 @@ int closingPosition = message.IndexOf(closeSpan);
 openingPosition += openSpan.Length;
 int length = closingPosition - openingPosition;
 
-Console.WriteLine(message.Substring(openingPosition, length));
+Console.WriteLine(message.Substring(openingPosition, length)); */
+
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+while (true) 
+{
+    int openingPosition = message.IndexOf('(');
+    if
+        (openingPosition == -1)
+        break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
 
 Console.ReadKey();
